1913796d4fa55c0b2dfcfdccefd4c78e8874c018
rc420head <dennislebhardt@yahoo.com>
2014-10-15 21:23:15 -0500

    Add the sweetness

--- candykat.mk
+++ candykat.mk
@@ -1,12 +1,12 @@
 $(call inherit-product, device/lge/ls990/full_ls990.mk)
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/candykat/config/common_full_phone.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/candykat/config/nfc_enhanced.mk)
 
-PRODUCT_NAME := cm_ls990
+PRODUCT_NAME := candykat_ls990
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_DEVICE="g3" \
